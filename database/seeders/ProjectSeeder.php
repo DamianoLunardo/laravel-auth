03588966c7af3a0349f73e0a6e16436332f4ba92
DamianoL <damianolunardo@hotmail.it>
2023-12-21 14:39:59 +0100

    ADD - model Project and Controller

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Project;
+use Illuminate\Support\Str;
+
+class ProjectSeeder extends Seeder
+{
+    public function run(Faker $faker)
+    {
+        
+    }
+}
